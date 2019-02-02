2f999ea02c3e5b0a9ef2281483067436a9ab323f
aalokhin <n.alokhina@gmail.com>
2019-02-02 18:09:58 +0200

    hmm

---
+++ src/defPack/JetPlane.java
@@ -0,0 +1,23 @@
+package defPack;
+
+/**
+ * Created by aalokhin on 2/2/19.
+ */
+public class JetPlane extends Aircraft
+{
+    private WeatherTower weatherTower;
+
+    JetPlane(String name, Coordinates coordinates)
+    {
+        super(name, coordinates);
+    }
+    public void updateConditions()
+    {
+
+    }
+    public void registerTower(WeatherTower weatherTower)
+    {
+
+    }
+}
+
